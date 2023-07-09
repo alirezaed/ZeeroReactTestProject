10ab3e4d5a59186007e2f1d84c73dc2eb04f317f
a.edalatpour <a.edalatpour@gmail.com>
2023-07-09 12:51:20 +0330

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((c) => c.json())
+      .then((c) => setData(c));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table style={{ borderCollapse: "collapse" }}>
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Price</th>
+            <th>Category</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map(({ id, title, price, category, image }) => (
+            <tr key={id}>
+              <td>{title}</td>
+              <td>{price}</td>
+              <td>{category}</td>
+              <td>
+                <img alt={title} style={{ height: "50px" }} src={image} />
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
